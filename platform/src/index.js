cbe847b9c5e2b9c7f2176e6927cf2c8841be2f90
Nese Yildirim <neseyild@student.arteveldehs.be>
2023-05-13 08:58:44 +0200

    add bootstrap

--- platform/src/index.js
+++ platform/src/index.js
@@ -5,6 +5,9 @@ import App from './app';
 import {BrowserRouter} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
